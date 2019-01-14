09408876baedd678338d0854056b7f1f618568cb
g.assies <gertjan.assies@gmail.com>
2019-01-14 08:54:11 +0100

    Added api endpoint to get power consumed and tariff info

--- lib/smartmeter_web/router.ex
+++ lib/smartmeter_web/router.ex
@@ -17,7 +17,6 @@ defmodule SmartmeterWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get  "/",     PageController,   :index
-    get  "/status", PageController, :status
     get  "/conf", ConfigController, :index
     post "/conf", ConfigController, :submit
   end
